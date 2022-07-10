9f4bb9eeb71a6a2d3c857ae96787068dc458a024
Sergio Basurto <35089326+sebasurto@users.noreply.github.com>
2022-07-10 08:47:21 -0500

    Agregado evento para obtener una imagen
    Cambiar el nombre del botón en fxml de Guardar por Cargar y llamar al botón btCargar

--- galeriaEstructDatos/src/main/java/ec/edu/espol/controladores/VistaFotosGeneralController.java
+++ galeriaEstructDatos/src/main/java/ec/edu/espol/controladores/VistaFotosGeneralController.java
@@ -5,12 +5,19 @@
  */
 package ec.edu.espol.controladores;
 
+import TDAs.LinkedList;
+import ec.edu.espol.model.Fotografia;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
 /**
  * FXML Controller class
@@ -23,10 +30,32 @@ public class VistaFotosGeneralController implements Initializable {
     private ScrollPane spfotopequena;
     @FXML
     private ImageView ivfotoGrande;
-
+    @FXML
+    private Button btCargar;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        //Aquí buscaremos las imágenes para añadir al album
+        btCargar.setOnAction(event -> {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Buscar Imagen");
+
+        // Agregar filtros para facilitar la busqueda
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Images", "*.*"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+            
+        // Obtener la imagen seleccionada
+        File imgFile = fileChooser.showOpenDialog(null);
+
+        // Mostar la imagen
+        if (imgFile != null) {
+            Image image = new Image("file:" + imgFile.getAbsolutePath());
+            ivfotoGrande.setImage(image);
+        }
+    });
     }    
     
 }
